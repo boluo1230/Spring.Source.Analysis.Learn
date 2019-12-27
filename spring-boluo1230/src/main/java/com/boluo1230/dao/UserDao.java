5030fd5d93f807b001e03f5f92ea856d52a0304e
liubolun <liubolun@kingsoft.com>
2019-12-27 17:40:06 +0800

    添加测试module；修改context包引入

---
+++ spring-boluo1230/src/main/java/com/boluo1230/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.boluo1230.dao;
+
+/**
+ * @author Code Farmer
+ * @date 2019/12/27 15:47
+ */
+
+public class UserDao {
+
+	public void query() {
+		System.out.println("query");
+	}
+
+}
---
+++ spring-boluo1230/src/main/java/com/boluo1230/test/Test.java
@@ -0,0 +1,23 @@
+package com.boluo1230.test;
+
+import com.boluo1230.dao.UserDao;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author Code Farmer
+ * @date 2019/12/27 15:46
+ */
+public class Test {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext applicationContext =
+				new AnnotationConfigApplicationContext();
+		applicationContext.register(UserDao.class);
+		applicationContext.refresh();
+
+		UserDao userDao = applicationContext.getBean(UserDao.class);
+		userDao.query();
+	}
+
+}
